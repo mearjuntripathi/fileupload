d496e1d71d9262f6ad0bb611fe1068917f9f0985
Arjun Tripathi <mearjuntripathi@gmail.com>
2024-10-05 13:09:58 +0530

    upload

--- src/context/Firebase.jsx
+++ src/context/Firebase.jsx
@@ -4,14 +4,15 @@ import { getDatabase, set, ref, get, child } from "firebase/database"; // Import
 import { getStorage, ref as storageRef, uploadBytes, getDownloadURL } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC_ZmMkUB9f4bEX6pUSHpDeTAcRjFVYVvE",
-    authDomain: "learn-firebase-1f7d0.firebaseapp.com",
-    databaseURL: "https://learn-firebase-1f7d0-default-rtdb.firebaseio.com",
-    projectId: "learn-firebase-1f7d0",
-    storageBucket: "learn-firebase-1f7d0.appspot.com",
-    messagingSenderId: "257784656220",
-    appId: "1:257784656220:web:571d6a9c5ec2dd98cc1199",
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
 };
+
 
 const firebaseApp = initializeApp(firebaseConfig);
 const database = getDatabase(firebaseApp);
